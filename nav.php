1c1a33cff5f05217249377dfab15953d992f8599
Arya Raj <aryaaryaa123@gmail.com>
2018-03-20 09:30:10 +0530

    Delete nav.php

--- nav.php
+++
@@ -1,3 +0,0 @@
-<form method="post" action="llpayment1.php">
-		<input type='submit' <?php if($f==1) {?> style="display:block;" <?php } else { ?> style="display:none;" <?php } ?> id='submitbtn' name='save' value='Proceed To Payment' >
-	</form>
